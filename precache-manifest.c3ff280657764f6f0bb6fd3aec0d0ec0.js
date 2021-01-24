4d579e8adc869d271bdfc6762dc26c130f5b89e5
= <=>
2021-01-24 19:40:07 -0400

    Updates

---
+++ precache-manifest.c3ff280657764f6f0bb6fd3aec0d0ec0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c265f3186488dc41333dcfbf1ba3215",
+    "url": "/piso4/index.html"
+  },
+  {
+    "revision": "25bdb0955086a04e513d",
+    "url": "/piso4/static/css/main.5ed37565.chunk.css"
+  },
+  {
+    "revision": "3e0cb841b4f21e2ee189",
+    "url": "/piso4/static/js/2.fb555a9a.chunk.js"
+  },
+  {
+    "revision": "88c0bfd11a2be5419414edbddb842b0f",
+    "url": "/piso4/static/js/2.fb555a9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25bdb0955086a04e513d",
+    "url": "/piso4/static/js/main.1fe6fafc.chunk.js"
+  },
+  {
+    "revision": "eea1f6f47dc9f0adb698",
+    "url": "/piso4/static/js/runtime-main.1723f6ac.js"
+  },
+  {
+    "revision": "daa1deedb3037f4961475dc6c7fcb60b",
+    "url": "/piso4/static/media/RebeltonBold.daa1deed.ttf"
+  }
+]);
